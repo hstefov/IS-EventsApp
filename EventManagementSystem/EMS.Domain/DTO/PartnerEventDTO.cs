c9d33f006b64ad8e0511bc7fa8e897d2c301689f
janepanov <panovjane@gmail.com>
2024-09-13 21:28:20 +0200

    Display events from partner's app

---
+++ EventManagementSystem/EMS.Domain/DTO/PartnerEventDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Domain.DTO
+{
+    public class PartnerEventDTO
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public string? DefaultImageURL { get; set; } = "https://t4.ftcdn.net/jpg/01/75/46/69/360_F_175466970_aRDdYku348o2ytRIJgeTuqychevhUe7u.jpg";
+    }
+}
---
+++ EventManagementSystem/EMS.Web/Controllers/REST/PartnerEventsController.cs
@@ -0,0 +1,31 @@
+﻿using EMS.Domain.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace EMS.Web.Controllers.REST
+{
+    public class PartnerEventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            string URL = "https://web20240913191052.azurewebsites.net/api/PartnerStore/GetAllEvents";
+
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+            var data = response.Content.ReadAsAsync<List<PartnerEventDTO>>().Result;
+            return View(data);
+        }
+
+        public IActionResult Details(string id)
+        {
+            HttpClient client = new HttpClient();
+
+            string URL = "https://web20240913191052.azurewebsites.net/api/PartnerStore/GetDetailsForEvent/" + id;
+
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+            var data = response.Content.ReadAsAsync<PartnerEventDTO>().Result;
+            return View(data);
+        }
+    }
+}
