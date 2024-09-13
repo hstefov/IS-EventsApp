891df51b257c08cccb8c19f919306748d15df39c
Hristijan Stefov <kikostefov@gmail.com>
2024-09-13 18:25:52 +0200

    Add Events Api Controller

---
+++ EventManagementSystem/EMS.Web/Controllers/REST/EventsController.cs
@@ -0,0 +1,33 @@
+﻿using EMS.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EMS.Domain.Models;
+
+namespace EMS.Web.Controllers.REST
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+
+        private readonly IEventService _eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet("[action]")]
+        public List<Event> GetAllEvents()
+        {
+            return this._eventService.GetAllEvents();
+        }
+
+        [HttpGet("{id}")]
+        public Event GetDetailsForEvent(Guid id)
+        {
+            return this._eventService.GetDetailsForEvent(id);
+        }
+
+    }
+}
